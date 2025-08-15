5ad24558f885f4e7bf116ab1d2bc15a00beb4496
IPulsarS <67330965+IPulsarS@users.noreply.github.com>
2025-08-16 03:01:43 +0800

    Update rules.c

--- kernel/selinux/rules.c
+++ kernel/selinux/rules.c
@@ -140,9 +140,6 @@ void ksu_apply_kernelsu_rules()
 	ksu_allow(db, "system_server", KERNEL_SU_DOMAIN, "process", "getpgid");
 	ksu_allow(db, "system_server", KERNEL_SU_DOMAIN, "process", "sigkill");
 
-	// https://android-review.googlesource.com/c/platform/system/logging/+/3725346
-	ksu_dontaudit(db, ALL, KERNEL_SU_DOMAIN, "dir", "getattr");
-
 #ifdef CONFIG_KSU_SUSFS
 	// Allow umount in zygote process without installing zygisk
 	ksu_allow(db, "zygote", "labeledfs", "filesystem", "unmount");
